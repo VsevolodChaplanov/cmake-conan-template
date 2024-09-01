ac4114528a3f3d7a60d6906fc6daf4f0de4dd29d
Vsevolod <woornad@gmail.com>
2024-09-01 19:12:47 +0300

    fixes for fetch_content

---
+++ cmake/install-rules.cmake
@@ -0,0 +1,39 @@
+include_guard(GLOBAL)
+
+# find_package(<package>) call for consumers to find this project
+set(package ${PROJECT_NAME})
+
+include(CMakePackageConfigHelpers)
+include(GNUInstallDirs)
+
+install(TARGETS ${PROJECT_NAME} EXPORT ${PROJECT_NAME}Targets)
+
+write_basic_package_version_file(${package}ConfigVersion.cmake COMPATIBILITY SameMajorVersion ARCH_INDEPENDENT)
+
+# Allow package maintainers to freely override the path for the configs
+set(${package}_INSTALL_CMAKEDIR
+    ${CMAKE_INSTALL_DATADIR}/${package}
+    CACHE STRING "CMake package config location relative to the install prefix")
+set_property(CACHE ${package}_INSTALL_CMAKEDIR PROPERTY TYPE PATH)
+mark_as_advanced(${package}_INSTALL_CMAKEDIR)
+
+configure_package_config_file(cmake/install-config.cmake.in ${PROJECT_NAME}Config.cmake
+                              INSTALL_DESTINATION ${${package}_INSTALL_CMAKEDIR})
+
+install(FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}Config.cmake ${PROJECT_BINARY_DIR}/${PROJECT_NAME}ConfigVersion.cmake
+        DESTINATION ${${package}_INSTALL_CMAKEDIR})
+
+install(
+    FILES ${PROJECT_BINARY_DIR}/${package}ConfigVersion.cmake
+    DESTINATION ${${package}_INSTALL_CMAKEDIR}
+    COMPONENT ${package}_Development)
+
+install(
+    EXPORT ${package}Targets
+    NAMESPACE ${package}::
+    DESTINATION ${${package}_INSTALL_CMAKEDIR}
+    COMPONENT ${package}_Development)
+
+if(PROJECT_IS_TOP_LEVEL)
+    include(CPack)
+endif()
